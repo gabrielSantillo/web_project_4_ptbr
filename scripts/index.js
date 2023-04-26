c6e76bd0dc6251ba78aae8e3da563cc86f95b73a
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-04-25 21:09:56 -0400

    Added a function to close the popup when save

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,9 @@
 const closePopup = document.getElementById('close-button');
 const openPopup = document.querySelector('#edit-profile');
 const popupSection = document.querySelector('#popup');
+const popupSaveButton = document.getElementById('save-button');
+const profileName = document.getElementById('profile-name');
+const profileAbout = document.getElementById('profile-about');
 
 function handleOpenPopup() {
     popupSection.classList.add('popup-opened');
@@ -10,7 +13,17 @@ function handleClosePopup() {
     popupSection.classList.remove('popup-opened');
 }
 
+function handleSaveButton() {
+    let name = document.getElementById('name');
+    let about = document.getElementById('about')
+
+    
+
+    handleClosePopup();
+}
+
 
 
 openPopup.addEventListener('click', handleOpenPopup);
 closePopup.addEventListener('click', handleClosePopup);
+popupSaveButton.addEventListener('click', handleSaveButton);
