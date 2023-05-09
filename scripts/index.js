e7e88011e1088ce915f3994b4d5b8b147ebfc847
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-05-09 18:56:10 -0400

    Adding during the elemnts render the function that will remove and like a post

--- scripts/index.js
+++ scripts/index.js
@@ -1,127 +1,137 @@
-const closePopup = document.getElementById('close-button');
-const openPopup = document.querySelector('#edit-profile');
+const closePopup = document.getElementById("close-button");
+const openPopup = document.querySelector("#edit-profile");
 
-const popupSection = document.querySelector('#popup');
-const popupSaveButton = document.getElementById('save-button');
-const profileName = document.getElementById('profile-name');
-const profileAbout = document.getElementById('profile-about');
+const popupSection = document.querySelector("#popup");
+const popupSaveButton = document.getElementById("save-button");
+const profileName = document.getElementById("profile-name");
+const profileAbout = document.getElementById("profile-about");
 
-const elementWithAllImages = document.querySelector('.post');
+const elementWithAllImages = document.querySelector(".post");
 
-const addPost = document.querySelector('#add-post');
-const popupAddPost = document.querySelector('#popup-add-post');
+const addPost = document.querySelector("#add-post");
+const popupAddPost = document.querySelector("#popup-add-post");
 const closePopupPost = document.querySelector("#close-button-post");
 const savePostButton = document.querySelector("#save-button-post");
 
-
 const images = [
-    {
-      title: "Vale de Yosemite",
-      url: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg"
-    },
-    {
-      title: "Lago Louise",
-      url: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg"
-    },
-    {
-      title: "Montanhas Carecas",
-      url: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg"
-    },
-    {
-      title: "Latemar",
-      url: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg"
-    },
-    {
-      title: "Parque Nacional da Vanoise ",
-      url: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg"
-    },
-    {
-      title: "Lago di Braies",
-      url: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg"
-    }
-  ];
+  {
+    title: "Vale de Yosemite",
+    url: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
+  },
+  {
+    title: "Lago Louise",
+    url: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
+  },
+  {
+    title: "Montanhas Carecas",
+    url: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
+  },
+  {
+    title: "Latemar",
+    url: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
+  },
+  {
+    title: "Parque Nacional da Vanoise ",
+    url: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
+  },
+  {
+    title: "Lago di Braies",
+    url: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
+  },
+];
 
 function handleOpenPopup(evt) {
-  if(evt.target.id === 'add-post') {
+  if (evt.target.id === "add-post") {
     popupAddPost.classList.add("popup-opened");
   } else {
-    popupSection.classList.add('popup-opened');
-  } 
+    popupSection.classList.add("popup-opened");
+  }
 }
 
 function handleClosePopup(evt) {
-  if(evt.target.id === 'close-button-post' || evt.target.id === 'save-button-post') {
+  if (
+    evt.target.id === "close-button-post" ||
+    evt.target.id === "save-button-post"
+  ) {
     popupAddPost.classList.remove("popup-opened");
   } else {
-    popupSection.classList.remove('popup-opened');
+    popupSection.classList.remove("popup-opened");
   }
-    
 }
 
 function handleSaveButton(evt) {
-    evt.preventDefault();
-    
-    let name = document.getElementById('name');
-    let about = document.getElementById('about');
-
-    if(name.value === "" || about.value === "") {
-        return alert("Please, complete the entire form before saving.")
-    }
-
-    profileName.textContent = name.value;
-    profileAbout.textContent = about.value;
-    
-    handleClosePopup(evt);
-}
-
-
-function renderPostCard(post) {
-    const postTemplate = document.querySelector('#template').content;
-    const postElement = postTemplate.querySelector('.post__card').cloneNode(true);
-
-    postElement.querySelector('.post__card-image').setAttribute('src', post.url);
-    postElement.querySelector('.post__card-image').setAttribute('alt', `Imagem de ${post.url}`);
-    postElement.querySelector('.post__card-content-title').textContent = post.title;
-
-    return postElement;
-}
-
-for (const image of images) {
-    const postCreated = renderPostCard(image);
-    elementWithAllImages.append(postCreated)
-}
-
-
-function handlePost(evt) {
   evt.preventDefault();
 
-  const postTitle = document.querySelector('#post-title');
-  const postImageUrl = document.querySelector('#post-image-url');
+  let name = document.getElementById("name");
+  let about = document.getElementById("about");
 
-  if(postTitle === "" || postImageUrl === "") {
-    return alert("Por favor, preencha todos os campos");
+  if (name.value === "" || about.value === "") {
+    return alert("Please, complete the entire form before saving.");
   }
 
-  elementWithAllImages.prepend(renderPostCard({
-    title: postTitle.value,
-    url: postImageUrl.value
-  }))
+  profileName.textContent = name.value;
+  profileAbout.textContent = about.value;
 
   handleClosePopup(evt);
 }
 
+function renderPostCard(post) {
+  const postTemplate = document.querySelector("#template").content;
+  const postElement = postTemplate.querySelector(".post__card").cloneNode(true);
 
-openPopup.addEventListener('click', handleOpenPopup);
-closePopup.addEventListener('click', handleClosePopup);
+  postElement.querySelector(".post__card-image").setAttribute("src", post.url);
+  postElement
+    .querySelector(".post__card-image")
+    .setAttribute("alt", `Imagem de ${post.url}`);
+  postElement.querySelector(".post__card-content-title").textContent =
+    post.title;
 
-popupSaveButton.addEventListener('click', handleSaveButton);
+  postElement
+    .querySelector(".post__card-content-like")
+    .addEventListener("click", (evt) => {
+      evt.target.setAttribute("src", "./images/post/post-like-filled.png");
+    });
 
-addPost.addEventListener('click', handleOpenPopup);
-closePopupPost.addEventListener('click', handleClosePopup);
+  postElement
+    .querySelector(".post__card-remove")
+    .addEventListener("click", (evt) => {
+      evt.target.parentElement.remove();
+    });
+
+  return postElement;
+}
+
+for (const image of images) {
+  const postCreated = renderPostCard(image);
+  elementWithAllImages.append(postCreated);
+}
+
+function handlePost(evt) {
+  evt.preventDefault();
+
+  const postTitle = document.querySelector("#post-title");
+  const postImageUrl = document.querySelector("#post-image-url");
+
+  if (postTitle === "" || postImageUrl === "") {
+    return alert("Por favor, preencha todos os campos");
+  }
+
+  elementWithAllImages.prepend(
+    renderPostCard({
+      title: postTitle.value,
+      url: postImageUrl.value,
+    })
+  );
+
+  handleClosePopup(evt);
+}
+
+openPopup.addEventListener("click", handleOpenPopup);
+closePopup.addEventListener("click", handleClosePopup);
+
+popupSaveButton.addEventListener("click", handleSaveButton);
+
+addPost.addEventListener("click", handleOpenPopup);
+closePopupPost.addEventListener("click", handleClosePopup);
 
 savePostButton.addEventListener("click", handlePost);
-
-const removeButton = document.querySelector(".post__card-remove");
-removeButton.addEventListener('click', (evt) => {
-  evt.target.parentElement.remove();
-})
