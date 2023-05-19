dfade03c23b5c699ff0f7921aa0eef304d46c521
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-05-19 17:30:12 -0400

    updating the name of the class for hidding ans showing the message error

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,12 +1,12 @@
 const showInputError = (formElement, inputElement, errorMessage) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  const errorElement = formElement.querySelector(`.input__${inputElement.id}-error`);
   inputElement.classList.add("form__input_type_error");
   errorElement.textContent = errorMessage;
   errorElement.classList.add("form__input-error_active");
 };
 
 const hideInputError = (formElement, inputElement) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  const errorElement = formElement.querySelector(`.input__${inputElement.id}-error`);
   inputElement.classList.remove("form__input_type_error");
   errorElement.classList.remove("form__input-error_active");
   errorElement.textContent = "";
