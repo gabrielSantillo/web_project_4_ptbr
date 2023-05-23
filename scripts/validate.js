5ed98dd1079e75fcb74b59edb4adc7678a9c9669
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-05-23 14:40:19 -0400

    Refactoring the classes name

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,12 +1,12 @@
 const showInputError = (formElement, inputElement, errorMessage) => {
-  const errorElement = formElement.querySelector(`.input__${inputElement.id}-error`);
+  const errorElement = formElement.querySelector(`.popup__container-form-texts-${inputElement.id}-error`);
   inputElement.classList.add("form__input_type_error");
   errorElement.textContent = errorMessage;
   errorElement.classList.add("form__input-error_active");
 };
 
 const hideInputError = (formElement, inputElement) => {
-  const errorElement = formElement.querySelector(`.input__${inputElement.id}-error`);
+  const errorElement = formElement.querySelector(`.popup__container-form-texts-${inputElement.id}-error`);
   inputElement.classList.remove("form__input_type_error");
   errorElement.classList.remove("form__input-error_active");
   errorElement.textContent = "";
