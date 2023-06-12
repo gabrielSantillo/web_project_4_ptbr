870797c0ce969605ead4c469a554516412006650
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-06-12 13:37:58 -0400

    Starting to add the FormValidator class

---
+++ scripts/FormValidator.js
@@ -0,0 +1,14 @@
+const config = {
+  formSelector: ".form",
+  fieldsetList: ".form__set",
+  formInput: ".form__input",
+  submitButtonSelector: ".form__submit",
+  inactiveButtonClass: "button_inactive",
+  inputErrorClass: "popup__container-form-texts-",
+};
+
+class FormValidator {
+    constructor(data, formSelection) {
+        
+    }
+}
