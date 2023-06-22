58e92c7b162238966f7d78d97339de902395c473
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-06-22 14:02:03 -0400

    adding the UserInfo class

---
+++ scripts/UserInfo.js
@@ -0,0 +1,14 @@
+export default class UserInfo {
+    constructor({ data }) {
+        this._name = data.name;
+        this._description = data.description;
+    }
+
+    getUserInfo() {
+
+    }
+
+    setUserInfo() {
+        
+    }
+}
